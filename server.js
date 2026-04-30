2ac1d3d39c75590b88ee5955b48554bc8beb54df
dikiwi <dikiwi@jinrongdeMacBook-Pro-3.local>
2026-04-30 10:33:08 +0800

    u

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { VocabDB, REVIEW_INTERVALS, KIDS_REVIEW_INTERVALS } = require('./db');
 const logger = require('./logger');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 // 初始化数据库
 const db = new VocabDB(path.join(__dirname, 'vocab.db'));
